3693250097cbb9b76e9866977b7ecbe8aa1c7ec9
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2018-03-17 01:48:27 +0900

    first commit

---
+++ _social.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     _social
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     _social
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         _social
+ */
+
+// Your code starts here.
