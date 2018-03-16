d3ac0a0b13905966a17cd75a394b855948eea934
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2018-03-17 01:52:59 +0900

    prepare

--- _social.php
+++ _social.php
@@ -1,10 +1,10 @@
 <?php
 /**
  * Plugin Name:     _social
- * Plugin URI:      PLUGIN SITE HERE
- * Description:     PLUGIN DESCRIPTION HERE
- * Author:          YOUR NAME HERE
- * Author URI:      YOUR SITE HERE
+ * Plugin URI:      https://github.com/miya0001/_social
+ * Description:     A WordPress plugin to share the contents
+ * Author:          Takayuki Miyauchi
+ * Author URI:      https://miya.io/
  * Text Domain:     _social
  * Domain Path:     /languages
  * Version:         0.1.0
