40b4931218df6ea920e617890039f66d79dfd9f2
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2018-03-17 05:27:29 +0900

    add tests

--- src/OGP.php
+++ src/OGP.php
@@ -4,8 +4,16 @@ namespace _Social;
 
 class OGP
 {
+	/**
+	 * @var $post \WP_Post
+	 */
+	private $post;
+
 	public function __construct()
 	{
+		if ( is_singular() ) {
+			$this->post = get_post( get_the_ID() );
+		}
 	}
 
 	public static function get_instance()
@@ -27,22 +35,12 @@ class OGP
 			echo $this->og_tag( 'og:description', esc_attr( get_bloginfo( 'description' ) ) );
 			echo $this->og_tag( 'og:url', esc_url( home_url( '/' ) ) );
 		} elseif ( is_singular() ) {
-			$post = get_post( get_the_ID() );
+			$post = $this->post;
 			echo $this->og_tag( 'og:type', 'article' );
 			echo $this->og_tag( 'og:site_name', esc_attr( get_bloginfo( 'name' ) ) );
 			echo $this->og_tag( 'og:title', esc_attr( get_the_title() ) );
 			echo $this->og_tag( 'og:url', esc_url( get_permalink() ) );
-			if ( ! post_password_required() ) {
-				if ( ! empty( $post->post_excerpt ) ) {
-					$content = preg_replace( '@https?://[\S]+@', '',
-								strip_shortcodes( wp_kses( $post->post_excerpt, array() ) ) );
-				} else {
-					$exploded_content_on_more_tag = explode( '<!--more-->', $post->post_content );
-					$content = wp_trim_words( preg_replace( '@https?://[\S]+@', '',
-								strip_shortcodes( wp_kses( $exploded_content_on_more_tag[0], array() ) ) ) );
-				}
-				echo $this->og_tag( 'og:description', $content );
-			}
+			echo $this->og_tag( 'og:description', $this->get_the_og_description() );
 			echo $this->og_tag( 'article:published_time',
 					date( 'c', strtotime( $post->post_date_gmt ) ) );
 			echo $this->og_tag( 'article:modified_time',
@@ -56,6 +54,7 @@ class OGP
 	/**
 	 * @param string $property
 	 * @param string $content
+	 *
 	 * @return string The Open Graph Tag.
 	 */
 	private function og_tag( $property, $content )
@@ -66,6 +65,27 @@ class OGP
 		}
 
 		return "";
+	}
+
+	/**
+	 * @return string The description of the post.
+	 */
+	private function get_the_og_description()
+	{
+		if ( ! post_password_required() ) {
+			$post = $this->post;
+			if ( ! empty( $post->post_excerpt ) ) {
+				$content = preg_replace( '@https?://[\S]+@', '',
+					strip_shortcodes( wp_kses( $post->post_excerpt, array() ) ) );
+			} else {
+				$exploded_content_on_more_tag = explode( '<!--more-->', $post->post_content );
+				$content = wp_trim_words( preg_replace( '@https?://[\S]+@', '',
+					strip_shortcodes( wp_kses( $exploded_content_on_more_tag[0], array() ) ) ) );
+			}
+			return $content;
+		}
+
+		return '';
 	}
 
 	/**
--- tests/test-social.php
+++ tests/test-social.php
@@ -8,13 +8,38 @@
 /**
  * Sample test case.
  */
-class SampleTest extends WP_UnitTestCase {
+class SampleTest extends WP_UnitTestCase
+{
+	public function test_og_description()
+	{
+		$post = $this->factory()->post->create_and_get( array(
+			'post_title' => 'Hello World',
+			'post_excerpt' => 'Hi! Are you happy?',
+		) );
 
-	/**
-	 * A single example test.
-	 */
-	function test_sample() {
-		// Replace this with some actual testing code.
-		$this->assertTrue( true );
+		$this->go_to( get_permalink( $post->ID ) );
+
+		$ogp = _Social\OGP::get_instance();
+		$method = new ReflectionMethod( get_class( $ogp ), 'get_the_og_description' );
+		$method->setAccessible( true );
+		$res = $method->invoke( $ogp );
+		$this->assertSame( "Hi! Are you happy?", $res );
+	}
+
+	public function test_og_description_with_no_excerpt()
+	{
+		$post = $this->factory()->post->create_and_get( array(
+			'post_title' => 'Hello World',
+			'post_content' => 'Hi! Are you happy?',
+			'post_excerpt' => '',
+		) );
+
+		$this->go_to( get_permalink( $post->ID ) );
+
+		$ogp = _Social\OGP::get_instance();
+		$method = new ReflectionMethod( get_class( $ogp ), 'get_the_og_description' );
+		$method->setAccessible( true );
+		$res = $method->invoke( $ogp );
+		$this->assertSame( "Hi! Are you happy?", $res );
 	}
 }
