e854c81b24b2a29e3681e9003ea6f7a5fc849830
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2018-03-18 00:45:13 +0900

    add some tests

--- tests/test-social.php
+++ tests/test-social.php
@@ -42,4 +42,25 @@ class SampleTest extends WP_UnitTestCase
 		$res = $method->invoke( $ogp );
 		$this->assertSame( "Hi! Are you happy?", $res );
 	}
+
+	public function test_get_data()
+	{
+		$post = $this->factory()->post->create_and_get( array(
+			'post_title' => 'Hello World',
+			'post_content' => 'Hi! Are you happy?',
+			'post_excerpt' => '',
+		) );
+
+		$obj = _Social\Share\OGP::get_instance();
+		$method = new ReflectionMethod( get_class( $obj ), 'get_data' );
+		$method->setAccessible( true );
+
+		$this->go_to( get_permalink( $post->ID ) );
+		$res = $method->invoke( $obj );
+		$this->assertSame( 10, count( array_keys( $res ) ) );
+
+		$this->go_to( '/' );
+		$res = $method->invoke( $obj );
+		$this->assertSame( 7, count( array_keys( $res ) ) );
+	}
 }
