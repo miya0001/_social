29856ff37875101a2624f3dc984b3c572abf432e
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2018-03-18 02:14:56 +0900

    test for twitter cards

--- tests/test-social.php
+++ tests/test-social.php
@@ -58,9 +58,34 @@ class SampleTest extends WP_UnitTestCase
 		$this->go_to( get_permalink( $post->ID ) );
 		$res = $method->invoke( $obj );
 		$this->assertSame( 10, count( array_keys( $res ) ) );
+		$this->assertSame( 'Hello World', $res['og:title'] );
 
 		$this->go_to( '/' );
 		$res = $method->invoke( $obj );
 		$this->assertSame( 7, count( array_keys( $res ) ) );
+		$this->assertSame( 'Test Blog', $res['og:title'] );
+	}
+
+	public function test_twitter_card()
+	{
+		$post = $this->factory()->post->create_and_get( array(
+			'post_title' => 'Hello World',
+			'post_content' => 'Hi! Are you happy?',
+			'post_excerpt' => '',
+		) );
+
+		$obj = _Social\Share\Twitter::get_instance();
+		$method = new ReflectionMethod( get_class( $obj ), 'get_the_card' );
+		$method->setAccessible( true );
+
+		$this->go_to( get_permalink( $post->ID ) );
+		$res = $method->invoke( $obj );
+		$this->assertSame( 'summary_large_image', $res );
+
+		update_option( _Social::get_instance()->get_prefix(), array(
+			'twitter-card' => 'summary',
+		) );
+		$res = $method->invoke( $obj );
+		$this->assertSame( 'summary', $res );
 	}
 }
