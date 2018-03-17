a42a934cf00976b8c36ad44404c571f32a40b1a0
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2018-03-18 02:00:04 +0900

    add default image

--- src/_Social/Admin.php
+++ src/_Social/Admin.php
@@ -31,6 +31,7 @@ final class Admin
 	{
 		add_action( 'admin_menu', array( $this, 'admin_menu' ) );
 		add_action( 'admin_init', array( $this, 'admin_init' ) );
+		add_action( 'admin_footer', array( $this, 'admin_footer' ) );
 	}
 
 	public function admin_menu() {
@@ -47,10 +48,25 @@ final class Admin
 	{
 		register_setting( '_social-settings', $this->prefix );
 
+		add_settings_section( 'general-settings', 'General Settings', null, $this->prefix );
 		add_settings_section( 'ogp-settings', 'Facebook Open Graph', null, $this->prefix );
 		add_settings_section( 'twitter-settings', 'Twitter Cards', function() {
 			echo '<p>The setting section for the <a href="https://dev.twitter.com/web/sign-inhttps://dev.twitter.com/cards/overview.html">Twitter Cards</a>.</p>';
 		}, $this->prefix );
+
+		add_settings_field(
+			'default-image',
+			'Default Image',
+			function() {
+				$value = esc_attr( \_Social::get_instance()->get_option( 'default-image' ) );
+				$prefix = esc_attr( $this->prefix );
+				echo "<div id='image-preview' style='width: 100%; height: 315px; border: 1px solid #cccccc; background-size: cover; background-position: center center; max-width: 600px;'></div>";
+				echo "<p><input type='text' id='default-image' name='{$prefix}[default-image]' value='$value' style='width: 100%; max-width: 600px;' /></p>";
+				echo "<p class=\"description\" style=\"width: auto;\">Use images that are at least 1200 x 630 pixels for the best display on high resolution devices. At the minimum, you should use images that are 600 x 315 pixels to display link page posts with larger images. Images can be up to 8MB in size.</p>";
+			},
+			$this->prefix,
+			'general-settings'
+		);
 
 		add_settings_field(
 			'app_id',
@@ -101,6 +117,28 @@ final class Admin
 			$this->prefix,
 			'twitter-settings'
 		);
+	}
+
+	public function admin_footer()
+	{
+		if ( 'options-general.php' !== $GLOBALS['pagenow'] || empty( $_GET['page'] ) || '_social' !== $_GET['page'] ) {
+			return;
+		}
+
+		?>
+		<script>
+			( function( $ ) {
+				var image = $( '#default-image' );
+				image.on( 'change', function() {
+					$( '#image-preview' ).css( 'backgroundImage', 'url(' + $(this).val() + ')' );
+				} );
+
+				if ( image.val() ) {
+					$( '#image-preview' ).css( 'backgroundImage', 'url(' + image.val() + ')' );
+				}
+			} )( jQuery );
+		</script>
+		<?php
 	}
 
 	public function display()
