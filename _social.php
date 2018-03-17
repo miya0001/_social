5a805f9fbdd7d8c85d6ba25ea9aaffebf80f472e
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2018-03-17 18:16:42 +0900

    add admin panel

--- _social.php
+++ _social.php
@@ -15,15 +15,45 @@
 // Autoload
 require_once( dirname( __FILE__ ) . '/vendor/autoload.php' );
 
-add_action( 'init', function() {
-	$plugin_slug = plugin_basename( __FILE__ );
-	$gh_user = 'miya0001';
-	$gh_repo = '_social';
+class _Social
+{
+	public function __construct()
+	{
+	}
 
-	new Miya\WP\GH_Auto_Updater( $plugin_slug, $gh_user, $gh_repo );
-} );
+	public static function get_instance()
+	{
+		static $instance;
+		if ( ! $instance ) {
+			$instance = new _Social();
+		}
+		return $instance;
+	}
 
-add_action( 'wp_head', function() {
-	_Social\OGP::get_instance()->display();
-	_Social\Twitter::get_instance()->display();
-} );
+	public function register()
+	{
+		if ( is_admin() ) {
+			_Social\Admin::get_instance()->register();
+		}
+
+		add_action( 'init', array( $this, 'init' ) );
+		add_action( 'wp_head', array( $this, 'wp_head' ) );
+	}
+
+	public function init()
+	{
+		$plugin_slug = plugin_basename( __FILE__ );
+		$gh_user = 'miya0001';
+		$gh_repo = '_social';
+
+		new Miya\WP\GH_Auto_Updater( $plugin_slug, $gh_user, $gh_repo );
+	}
+
+	public function wp_head()
+	{
+		_Social\OGP::get_instance()->display();
+		_Social\Twitter::get_instance()->display();
+	}
+}
+
+_Social::get_instance()->register();
